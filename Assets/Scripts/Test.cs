527ab35e35f35229384369b96bf27359200c0d70
SongShu <1652954510@qq.com>
2024-05-07 09:36:41 +0800

    ABSystem-master

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Tangzx.ABSystem;
+
+public class Test : MonoBehaviour
+{
+    AssetBundleManager manager;
+
+    void Start()
+    {
+        manager = gameObject.AddComponent<AssetBundleManager>();
+        manager.Init(() =>
+        {
+            LoadObjects();
+        });
+    }
+
+    void LoadObjects()
+    {
+        manager.Load("Assets.Prefabs.Sphere.prefab", (a) =>
+        {
+            GameObject go = Instantiate(a.mainObject) as GameObject;//a.Instantiate();
+            go.transform.localPosition = new Vector3(3, 3, 3);
+        });
+        //manager.Load("Assets.Prefabs.Cube.prefab.ab", (a) =>
+        //{
+        //    GameObject go = a.Instantiate();
+        //    go.transform.localPosition = new Vector3(6, 3, 3);
+        //});
+        //manager.Load("Assets.Prefabs.Plane.prefab.ab", (a) =>
+        //{
+        //    GameObject go = a.Instantiate();
+        //    go.transform.localPosition = new Vector3(9, 3, 3);
+        //});
+        //manager.Load("Assets.Prefabs.Capsule.prefab.ab", (a) =>
+        //{
+        //    GameObject go = a.Instantiate();
+        //    go.transform.localPosition = new Vector3(12, 3, 3);
+        //});
+    }
+}
---
+++ readme.md
@@ -0,0 +1,41 @@
+# 特点
+* 指定要打包的文件，程序会自动分析依赖、拆分打包粒度到最优方式打包
+* 缓存上次的打包信息，下次打包会增量打包
+* 自动管理卸载不用的AB
+* 可扩展，自定义打包和加载路径
+
+# 如何运行
+1. 通过`ABSystem/Builder Panel`打开配置界面
+2. 在配置界面中更改打包配置
+3. 点击`Build`执行打包
+4. 增加宏 `AB_MODE`
+5. 运行测试
+
+# 如何使用
+```c#
+
+void Start()
+{
+	AssetBundleManager manager = AssetBundleManager.Instance;
+	manager.Init(() =>
+    {
+        LoadObjects();
+    });
+}
+
+void LoadObjects()
+{
+    manager.Load("Assets.Prefabs.Sphere.prefab", (a) =>
+    {
+        GameObject go = a.Instantiate(); //自动管理：当go被Destroy时，AB会被释放回收
+    });
+
+    manager.Load("Assets.my_txture.png", (a) =>
+    {
+    	// a.Retain();	//强制引用计数加一
+    	// a.Release();	//引用计数减一
+    	Texture tex = a.Require(this); //自动管理：当this被Destroy时，AB会被释放回收
+    });
+}
+
+```
